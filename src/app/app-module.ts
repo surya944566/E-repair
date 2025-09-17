42f2ce44a1a65895de7cf26676cd8485a9735bc9
surya944566 <124070211+surya944566@users.noreply.github.com>
2025-09-17 13:06:30 +0530

    e-repair project init

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Navbar } from './navbar/navbar';
+import { Footer } from './footer/footer';
+import { Mainpage } from './mainpage/mainpage';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Navbar,
+    Footer,
+    Mainpage
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mainpage/mainpage.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mainpage',
+  standalone: false,
+  templateUrl: './mainpage.html',
+  styleUrl: './mainpage.css'
+})
+export class Mainpage {
+
+}
