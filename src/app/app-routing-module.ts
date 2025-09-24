2bfeeaa799a8c77186b139f4e8c9bbcc578641eb
surya944566 <124070211+surya944566@users.noreply.github.com>
2025-09-24 11:19:59 +0530

    admin page and router implementation

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Admin } from './admin/admin';
+import { Mainpage } from './mainpage/mainpage';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '',component: Mainpage},
+  {path: 'admin',component: Admin}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
